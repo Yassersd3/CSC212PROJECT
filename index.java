ba92b75dee2b353cf8b5ae8d8431fbda5ed810f3
Yassersd3 <yasersds222@gmail.com>
2024-11-22 17:44:57 +0300

    Update index.java

--- index.java
+++ index.java
@@ -1,39 +1,45 @@
 package CSC212_PROJECT;
 
-class Documents<T>{
-	Linkedlist<String> words=new Linkedlist<String>();
+class Documents<T> {
+	Linkedlist<String> words = new Linkedlist<String>();
 	int id;
-	public Documents(int id,Linkedlist<String> word) {
-		this.id=id;
-		words=word;
+
+	public Documents(int id, Linkedlist<String> word) {
+		this.id = id;
+		words = word;
 	}
 }
+
 public class index {
 
 	Linkedlist<Documents> alldocs;
+
 	public index() {
-		alldocs=new Linkedlist<Documents>();
+		alldocs = new Linkedlist<Documents>();
 	}
+
 	public void addDoc(Documents d) {
 		alldocs.insert(d);
+
 	}
+
 	public void displayDocs() {
-		if(alldocs.empty())
+		if (alldocs.empty())
 			System.out.println("Empty documents");
-		
+
 		alldocs.findfirst();
-		while(!alldocs.last()) {
-			Documents d=alldocs.retrieve();
+		while (!alldocs.last()) {
+			Documents d = alldocs.retrieve();
 			System.out.println("-----------------------------------------------");
-			System.out.println("id: "+d.id);
+			System.out.println("id: " + d.id);
 			d.words.display();
 			alldocs.findnext();
 		}
-		Documents d=alldocs.retrieve();
+		Documents d = alldocs.retrieve();
 		System.out.println("-----------------------------------------------");
-		System.out.println("id: "+d.id);
+		System.out.println("id: " + d.id);
 		d.words.display();
-		
+
 	}
 	public Documents getDocById(int id) {
 		alldocs.findfirst();
@@ -47,6 +53,9 @@ public class index {
 		return null;
 		
 	}
+
+	
+
 	
 
 }
